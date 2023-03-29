f485d46a172448258db4ebae38fcfac8857cf761
Ilia <iliamamageishvili@gmail.com>
2023-03-30 00:00:45 +0400

    Add tests for the Deposit function

---
+++ wallet/deposit_test.go
@@ -0,0 +1,34 @@
+package wallet
+
+import "testing"
+
+func TestDeposit(t *testing.T) {
+	t.Run("Empty wallet case", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		checkBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Non-empty wallet case", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		wallet.Deposit(Bitcoin(20))
+
+		checkBalance(t, wallet, Bitcoin(30))
+	})
+
+	t.Run("Negative wallet case", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(-10)}
+		wallet.Deposit(Bitcoin(20))
+
+		checkBalance(t, wallet, Bitcoin(10))
+	})
+}
+
+func checkBalance(t *testing.T, wallet Wallet, expected Bitcoin) {
+	t.Helper()
+
+	if wallet.balance != expected {
+		t.Errorf("expected %v but got %v", expected, wallet.balance)
+	}
+}
