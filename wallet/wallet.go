82bc070f2481617b145d09bac1f20430e5bc3ea3
Ilia <iliamamageishvili@gmail.com>
2023-03-29 13:18:43 +0400

    Implement the wallet with its functions

---
+++ wallet/wallet.go
@@ -0,0 +1,25 @@
+package wallet
+
+import "fmt"
+
+type Bitcoin float64
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return fmt.Errorf("Withdraw can't be done! Check the bal")
+	}
+	w.balance -= amount
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
