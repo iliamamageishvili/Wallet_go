89dac1b31f983d4dcdec3e5f27b1a2a633ad0067
Ilia <iliamamageishvili@gmail.com>
2023-03-31 19:25:21 +0400

    modift wallet to be conncurrent safe

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,17 +1,23 @@
 package wallet
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 type Bitcoin float64
 
 type Wallet struct {
 	balance Bitcoin
+	mu      sync.Mutex
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) error {
 	if amount < 0 {
 		return ErrNegativeInput
 	}
+	w.mu.Lock()
+	defer w.mu.Unlock()
 	w.balance += amount
 	return nil
 }
@@ -20,16 +26,20 @@ var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
 var ErrNegativeInput = errors.New("negative inputs are not allowed")
 
 func (w *Wallet) Withdraw(amount Bitcoin) error {
-	if amount > w.balance {
-		return ErrInsufficientFunds
-	}
 	if amount < 0 {
 		return ErrNegativeInput
+	}
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if amount > w.balance {
+		return ErrInsufficientFunds
 	}
 	w.balance -= amount
 	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
+	w.mu.Lock()
+	defer w.mu.Unlock()
 	return w.balance
 }
