12692e91cec23ecb9ed2485ec11cb58df88a0f9c
Ilia <iliamamageishvili@gmail.com>
2023-03-31 11:31:46 +0400

    adding test cases for deposit function

--- wallet/deposit_test.go
+++ wallet/deposit_test.go
@@ -23,6 +23,21 @@ func TestDeposit(t *testing.T) {
 
 		checkBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("Depositing zero", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		wallet.Deposit(Bitcoin(0))
+
+		checkBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Depositing negative value", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		wallet.Deposit(Bitcoin(-5))
+
+		checkBalance(t, wallet, Bitcoin(5))
+	})
+
 }
 
 func checkBalance(t *testing.T, wallet Wallet, expected Bitcoin) {
