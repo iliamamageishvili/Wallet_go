f1c225905e5515830952c5d212436973cba8f813
Ilia <iliamamageishvili@gmail.com>
2023-04-03 18:00:03 +0400

    split Deposit and Withdraw calls into separate goroutines

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -6,26 +6,33 @@ import (
 )
 
 func TestConcurrentSafe(t *testing.T) {
-
 	w := Wallet{}
 
 	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
+	n := 10
+	wg.Add(n * 2)
+
+	for i := 0; i < n; i++ {
 		go func() {
 			defer wg.Done()
 
 			for j := 0; j < 100; j++ {
 				err := w.Deposit(Bitcoin(1))
 				if err != nil {
-					t.Errorf("Wrong error: %v", err)
+					t.Errorf("Unexpected error: %v", err)
 				}
 			}
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
 
 			for j := 0; j < 50; j++ {
 				err := w.Withdraw(Bitcoin(1))
 				if err != nil {
-					t.Errorf("Wrong error: %v", err)
+					t.Errorf("Unexpected error: %v", err)
 				}
 			}
 		}()
@@ -35,6 +42,6 @@ func TestConcurrentSafe(t *testing.T) {
 
 	balance := w.Balance()
 	if balance != Bitcoin(500) {
-		t.Errorf("Wrong balance: %v", balance)
+		t.Errorf("Unexpected balance: %v", balance)
 	}
 }
